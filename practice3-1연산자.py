8f15e1e1b0b232da30a62468339fc7d53af5fb4a
LeeEunJun <76806640+LeeEumJun@users.noreply.github.com>
2021-01-02 00:52:29 +0900

    Add files via upload

---
+++ practice3-1연산자.py
@@ -0,0 +1,30 @@
+print(1+1)
+print(3-1)
+print(5*2)
+print(6/3)
+print(6/5)
+
+print(2**3)#2^3=8
+print(5%3)#나머지 구하기 =2
+print(10%3)
+print(5//3)#몫구하기
+print(10//3)
+
+
+print(10>3)#true
+print(4>=7)#false
+print(5<=5)#true
+
+print(3==3)#앞과 뒤가 같은가?=true
+print(4==3)#false
+print(3+4==7)#true
+
+print(1!=3)#앞과 뒤가 같지 않은가?=true
+print(not(1!=3))#false
+
+print((3>0)and(3<5))#true
+print((3>0)&(3<5))#true(and)
+print((3>0)or(3>5))#true
+print((3>0)|(3>5))#true(or)
+print(4>2>1)#true
+print(5>4>7)#true
